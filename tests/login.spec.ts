aa0828af7716e5ff3d9723877c6831f88c76f301
Alon Shulman <shulmanalon@gmail.com>
2025-04-08 11:29:28 +0300

    Added test steps to login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { loginUsers } from './test-data/users';
 import { MainPage } from '../pages/MainPage';
@@ -14,10 +14,18 @@ test.describe('login', () => {
     const mainPage = new MainPage(page);
     const loginPage = new LoginPage(page);
 
-    await mainPage.verifyLanding();
-    await mainPage.clickOnLoginSignup();
-    await loginPage.verifyLanding();
-    await loginPage.login(validUser.email, validUser.password);
-    await mainPage.verifySignedIn(validUser.fullName);
+    await test.step('Verify landing in main page, click on login and verify landing in login page', async () => {
+      await mainPage.verifyLanding();
+      await mainPage.clickOnLoginSignup();
+      await loginPage.verifyLanding();
+    });
+
+    await test.step('Fill valid user data and click to sign in', async () => {
+      await loginPage.login(validUser.email, validUser.password);
+    });
+
+    await test.step('Verify landing in main page and that user is signed in', async () => {
+      await mainPage.verifySignedIn(validUser.fullName);
+    });
   });
 });
